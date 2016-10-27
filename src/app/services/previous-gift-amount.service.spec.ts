93a707d4a25194032b2c8d2593a862a14dd71d6f
Samuel Mello <samueljmello@gmail.com>
2016-10-27 13:07:35 -0400

    Put corrected test name back in.

---
+++ src/app/services/previous-gift-amount.service.spec.ts
@@ -0,0 +1,54 @@
+import {TestBed, getTestBed, async, inject} from '@angular/core/testing';
+import {Headers, BaseRequestOptions, Response, HttpModule, Http, XHRBackend, RequestMethod} from '@angular/http';
+
+import {ResponseOptions} from '@angular/http';
+import {MockBackend, MockConnection} from '@angular/http/testing';
+import {PreviousGiftAmountService} from './previous-gift-amount.service';
+
+
+describe('Quick Donation Amounts Service', () => {
+
+    let mockBackend: MockBackend;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                PreviousGiftAmountService,
+                MockBackend,
+                BaseRequestOptions,
+                {
+                    provide: Http,
+                    deps: [MockBackend, BaseRequestOptions],
+                    useFactory: (backend: XHRBackend, defaultOptions: BaseRequestOptions) => {
+                        return new Http(backend, defaultOptions);
+                    }
+                }
+            ],
+            imports: [
+                HttpModule
+            ]
+        });
+
+        mockBackend = getTestBed().get(MockBackend);
+        TestBed.compileComponents();
+    }));
+
+    it('it should provide a previous gift amount',
+        async(inject([PreviousGiftAmountService], (previousGiftAmountService) => {
+            mockBackend.connections.subscribe(
+                (connection: MockConnection) => {
+                    connection.mockRespond(new Response(
+                        new ResponseOptions({
+                                body: 40
+                            }
+                        )));
+                });
+
+            previousGiftAmountService.get().subscribe(
+                (data) => {
+                    expect(data).toBe(40);
+                });
+        }))
+    );
+
+});
