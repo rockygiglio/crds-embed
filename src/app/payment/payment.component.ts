c6c7a19ac7aefbba262218279312949dd5c2aa12
Gabriel Smith <gabriel.smith01@gmail.com>
2016-10-31 15:25:15 -0400

    removed hardcoding parameter type

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -2,17 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import {ParameterService} from "../services/parameter.service";
 
 @Component({
-  selector: 'app-prototype',
+  selector: 'app-payment',
   templateUrl: 'payment.component.html',
   styleUrls: ['payment.component.css']
 })
 export class PaymentComponent implements OnInit {
 
-  constructor(private paramsService: ParameterService) {
-    this.paramsService.type = 'payment';
-  }
+  constructor(private paramsService: ParameterService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
