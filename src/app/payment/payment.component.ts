ef130dbdaa06fb90069daa89dce676be09f1bf55
Gabriel Smith <gabriel.smith01@gmail.com>
2016-11-01 22:17:17 -0400

    Code cleanup; Apply prototype functionality to payment

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Inject, Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
-import { ParameterService } from "../services/parameter.service";
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { GiftService } from "../services/gift.service";
+import { PrototypeStore } from "../prototype/prototype-state/prototype.store";
 
 @Component({
   selector: 'app-payment',
@@ -8,14 +10,72 @@ import { ParameterService } from "../services/parameter.service";
   styleUrls: ['payment.component.css']
 })
 export class PaymentComponent implements OnInit {
+  public form: FormGroup;
+  public customAmount: number;
+  public selectedAmount: string;
+  public amountDue: Array<Object>;
+  private PrototypeActions;
 
-  constructor(public params: ParameterService,
-              private location: Location) {
+
+  constructor(@Inject(PrototypeStore) private store: any,
+              private location: Location,
+              private gift: GiftService,
+              private fb: FormBuilder) {
   }
 
+
   ngOnInit() {
-    if (this.params.type === 'donation') {
+    if (this.gift.type === 'donation') {
       this.location.go('/donation')
+    }
+
+    this.amountDue = [
+      {
+        label: 'Minimum Due',
+        amount: this.gift.minPayment
+      },
+      {
+        label: 'Total Due',
+        amount: this.gift.totalCost
+      }
+    ];
+
+    this.form = this.fb.group({
+      amount: [this.gift.amount, [<any>Validators.required, this.validateAmount.bind(this)]],
+      customAmount: [this.gift.amount, [<any>Validators.required, this.validateAmount.bind(this)]],
+      selectedAmount: [this.gift.amount]
+    });
+  }
+
+  next() {
+    // this.store.dispatch(this.PrototypeActions.render('/auth'));
+    return false;
+  }
+
+  onCustomAmount(value) {
+    if (!isNaN(value)) {
+      delete(this.selectedAmount);
+      this.setAmount(value);
+    }
+  }
+
+  onSelectAmount(event, value) {
+    delete(this.customAmount);
+    this.setAmount(value);
+  }
+
+  setAmount(value) {
+    (<FormControl>this.form.controls['amount']).setValue(value, { onlySelf: true });
+    this.gift.amount = parseInt(value, 10);
+  }
+
+  isValid() {
+    return this.form.valid || this.gift.validAmount();
+  }
+
+  private validateAmount(control) {
+    return this.gift.validAmount() ? null : {
+      validateAmount: true
     }
   }
 
