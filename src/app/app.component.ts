210ed491715684d3814496709bc82b4237f6c548
Samuel Mello <samueljmello@gmail.com>
2016-11-03 11:24:07 -0400

    Fix for missing css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: '<div class="prototype-component inline-giving container"><router-outlet></router-outlet></div>',
-  styleUrls: ['../../styles/application.scss'],
+  styleUrls: ['../styles/application.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
