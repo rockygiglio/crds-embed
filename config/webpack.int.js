b1ddf5dc09642fda26f7f15f742f6854f1969a89
Samuel Mello <samueljmello@gmail.com>
2016-11-03 11:01:47 -0400

    Quick patch for https errors

---
+++ config/webpack.int.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+var webpackMerge = require('webpack-merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var commonConfig = require('./webpack.common.js');
+var helpers = require('./helpers');
+
+module.exports = webpackMerge(commonConfig, {
+  devtool: 'source-map',
+
+  output: {
+    path: helpers.root('dist'),
+    publicPath: '/',
+    filename: '[name].[hash].js',
+    chunkFilename: '[id].[hash].chunk.js'
+  },
+
+  htmlLoader: {
+    minimize: false
+  },
+
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ]
+});
