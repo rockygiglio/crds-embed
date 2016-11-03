be87ea1957ba376ba139edc22562162da9c05858
Samuel Mello <samueljmello@gmail.com>
2016-11-03 11:23:02 -0400

    Fix for CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from "@angular/forms";
-import { Store, StoreEnhancer, createStore } from "redux";
+import { ReactiveFormsModule } from '@angular/forms';
+import { Store, StoreEnhancer, createStore } from 'redux';
 
 import { AppComponent } from './app.component';
 import { routing, appRoutingProviders } from './app.routing';
@@ -10,14 +10,14 @@ import { PrototypeModule } from './prototype/prototype.module';
 import { DemoModule } from './demo/demo.module';
 
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { PaymentComponent } from "./payment/payment.component";
-import { DonationComponent } from "./donation/donation.component";
+import { PaymentComponent } from './payment/payment.component';
+import { DonationComponent } from './donation/donation.component';
 
-import { GiftService } from "./services/gift.service";
+import { GiftService } from './services/gift.service';
 
-import { GivingStore } from "./giving-state/giving.store";
-import { GivingState } from "./giving-state/giving.interfaces";
-import { givingReducer } from "./giving-state/giving.reducer";
+import { GivingStore } from './giving-state/giving.store';
+import { GivingState } from './giving-state/giving.interfaces';
+import { givingReducer } from './giving-state/giving.reducer';
 
 
 let devtools: StoreEnhancer<GivingState> =
