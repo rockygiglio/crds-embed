4aea84883a4c2a61d5a743ac5e04b2fca4473b65
ING062\sara.seissiger <sara.seissiger@ingagepartners.com>
2016-11-03 11:39:06 -0400

    US5481 using new httpclientservice

--- src/app/services/previous-gift-amount.service.ts
+++ src/app/services/previous-gift-amount.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers, Request, RequestOptions, RequestMethod } from '@angular/http';
+import { Response, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Resolve } from '@angular/router';
-import { CookieService } from 'angular2-cookie/core';
+import { HttpClientService } from './http-client.service';
+import { UserSessionService } from './user-session.service';
 
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
@@ -12,37 +13,25 @@ export class PreviousGiftAmountService implements Resolve<number> {
 
     private base = 'https://gatewayint.crossroads.net:443/gateway/api/';
     private url = this.base + 'donations';
-    private headers: Headers = new Headers();
-    private token: string = '';
 
-    constructor (private http: Http, private cookieService: CookieService) {
-        this.token = this.cookieService.get('sessionId');
-    }
+    constructor (private http: HttpClientService,
+                 private userSessionService: UserSessionService) {}
 
     resolve() {
-        return this.get();
+         return this.get();
     }
 
     get(): Observable<string> {
-
         let pre = this.url;
         this.url = pre + '?limit=1';
-        this.headers.append('Content-Type', 'application/json');
-        this.headers.append('Authorization', this.token);
 
-        let options = new RequestOptions({
-            method: RequestMethod.Get,
-            url: this.url,
-            headers: this.headers
-        });
-
-        return this.http.request(new Request(options))
+        return this.http.get(this.url)
             .map(this.extract)
             .catch(this.error);
     }
 
     private extract(res: Response) {
-        let body = res.json();
+        let body: any = res;
         let amount: string;
 
         if ( body.donations !== undefined && body.donations[0] !== undefined ) {
