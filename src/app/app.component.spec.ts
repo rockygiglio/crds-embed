6277f63e815c19c5597ba8a4f1cb9520b9196ae8
Samuel Mello <samueljmello@gmail.com>
2016-11-08 09:47:28 -0500

    US5109: Fix broken tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,32 @@
 /* tslint:disable:no-unused-variable */
 
-import { async, inject } from '@angular/core/testing';
+import { TestBed, async, inject } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Angulartics2 } from 'angulartics2';
+import { Angulartics2GoogleTagManager } from 'angulartics2/dist/providers';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('App: CrdsEmbed', () => {
 
+  let component: any;
+  let fixture: any;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AppComponent ],
+      providers:    [
+        Angulartics2,
+        Angulartics2GoogleTagManager
+      ],
+      imports: [
+        RouterTestingModule.withRoutes([])
+      ],
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
   it('should create an instance', () => {
-    let component: any = new AppComponent();
     expect(component).toBeTruthy();
   });
 
