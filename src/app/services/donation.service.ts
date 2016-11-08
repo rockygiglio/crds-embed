1f8fe67f92cfd13c52648b28a45e8aba1b742869
ING062\sara.seissiger <sara.seissiger@ingagepartners.com>
2016-11-08 16:37:36 -0500

    US5378 WIP to update in service from NG1 to NG2

---
+++ src/app/services/donation.service.ts
@@ -0,0 +1,141 @@
+import { Injectable } from '@angular/core';
+import { Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Resolve } from '@angular/router';
+import { HttpClientService } from './http-client.service';
+import { UserSessionService } from './user-session.service';
+import { PaymentService } from './payment.service';
+import { GiftService } from './gift.service';
+
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class DonationService implements Resolve<number> {
+
+  private transactionUrl = process.env.CRDS_API_ENDPOINT;
+
+  private bank: any;
+  private card: any;
+
+  constructor(private http: HttpClientService,
+    private userSessionService: UserSessionService,
+    private paymentService: PaymentService,
+    private giftService: GiftService) { }
+
+  resolve() {
+
+  }
+
+  // TODO needs to be called after input cc or bank details
+  submitTransactionInfo(giveForm) {
+    // TODO - where do these .then success and failure calls go?  To this.paymentService.getDonor()
+    if (giveForm.accountForm.$valid) {
+      this.paymentService.getDonor(this.giftService.email)
+        .then(function (donor) {
+          this.updateDonorAndDonate(donor.id);
+        },
+        function (error) {
+          this.createDonorAndDonate();
+        });
+    } else {
+      // TODO display general failure message on same page
+      // $rootScope.$emit('notify', $rootScope.MESSAGES.generalError);
+    }
+  }
+
+  updateDonorAndDonate(donorId) {
+    // The email below is only required for guest giver, however, there
+    // is no harm in sending it for an authenticated user as well,
+    // so we'll keep it simple and send it in all cases.
+    if (this.giftService.paymentType === 'cc') {
+      this.createCard();
+      this.paymentService.updateDonorWithCard(donorId, this.card, this.giftService.email)
+        .then(function (donor) {
+          this.donate();
+        }, this.paymentService.stripeErrorHandler);
+    } else if (this.giftService.paymentType === 'bank') {
+      this.createBank();
+      this.paymentService.updateDonorWithBankAcct(donorId, this.bank, this.giftService.email)
+        .then(function (donor) {
+          this.donate();
+        }, this.paymentService.stripeErrorHandler);
+    }
+    return this;
+  }
+
+  createDonorAndDonate() {
+    if (this.giftService.paymentType === 'cc') {
+      this.createCard();
+      this.paymentService.createDonorWithCard(this.card
+        , this.giftService.email
+        , 'TODO donorFirstName'
+        , 'TODO donorLastName')
+        .then(function (donor) {
+          this.donate();
+        }, this.paymentService.stripeErrorHandler);
+    } else if (this.giftService.paymentType === 'bank') {
+      this.createBank();
+      this.paymentService.createDonorWithBankAcct(this.bank
+        , this.giftService.email
+        , 'TODO donorFirstName'
+        , 'TODO donorLastName')
+        .then(function (donor) {
+          this.donate();
+        }, this.paymentService.stripeErrorHandler);
+    }
+  }
+
+  createBank() {
+    try {
+      this.bank = {
+        country: 'US',
+        currency: 'USD',
+        routing_number: this.giftService.routingNumber,
+        account_number: this.giftService.accountNumber,
+        account_holder_name: this.giftService.accountName,
+        account_holder_type: this.giftService.accountType
+      };
+    } catch (err) {
+      throw new Error('Unable to create bank account');
+    }
+
+  }
+
+  createCard() {
+    try {
+      this.card = {
+        name: 'TODO Name for credit card',
+        number: this.giftService.ccNumber,
+        exp_month: this.giftService.expDate.substr(0, 2),
+        exp_year: this.giftService.expDate.substr(2, 2),
+        cvc: this.giftService.cvv,
+        address_zip: this.giftService.zipCode
+      };
+    } catch (err) {
+      throw new Error('Unable to create credit card');
+    }
+  }
+
+  // TODO needs to be called from summary submit button (Pay button)
+  donate() {
+    this.postPayment();
+  }
+
+  postPayment(): Observable<any> {
+    let body = {
+      'amount': this.giftService.amount,
+      'pymt_type': this.giftService.paymentType,  // bank or cc
+      'transaction_type': 'PAYMENT',  // DONATION or PAYMENT
+      'invoice_id': this.giftService.invoiceId,
+    };
+
+    return this.http.post(this.transactionUrl, body)
+      .catch(this.handleError);
+  }
+
+  private handleError(res: Response) {
+    return [res.json()];
+  }
+
+}
---
+++ src/app/services/payment.service.ts
@@ -0,0 +1,134 @@
+import { Injectable } from '@angular/core';
+import { Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Resolve } from '@angular/router';
+import { HttpClientService } from './http-client.service';
+import { UserSessionService } from './user-session.service';
+
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class PaymentService {
+
+    private transactionUrl = process.env.CRDS_API_ENDPOINT;
+
+    // TODO set stripe KEY
+    // stripe.setPublishableKey(__STRIPE_PUBKEY__);
+
+    constructor(private http: HttpClientService,
+        private userSessionService: UserSessionService) { }
+
+    resolve() {
+    }
+
+    // getDonorOLD(email) {
+    //     let encodedEmail = email ?
+    //         encodeURI(email).replace(/\+/g, '%2B')
+    //         :
+    //         '';
+    //     let def = $q.defer();
+    //     $http({
+    //         method: 'GET',
+    //         url: this.transactionUrl + 'api/donor/?email=' + encodedEmail,
+    //         headers: {
+    //             Authorization: $cookies.get(cookieNames.SESSION_ID)
+    //         }
+    //     }).success(function (data) {
+    //         def.resolve(data);
+    //     }).error(function (response, statusCode) {
+    //         def.reject(_addGlobalErrorMessage(response.error, statusCode));
+    //     });
+
+    //     return def.promise;
+    // }
+
+    getDonor(email): Observable<string> {
+        let encodedEmail = email ?
+            encodeURI(email).replace(/\+/g, '%2B')
+            :
+            '';
+
+        return this.http.get(this.transactionUrl + 'api/donor/?email=' + encodedEmail)
+            .map(this.extractDonorEmail)
+            .catch(this.errorDonorEmail);
+    }
+
+    createDonorWithBankAcct(bankAcct, email, firstName, lastName) {
+        return this.apiDonor(bankAcct, email, firstName, lastName, stripe.bankAccount, 'POST');
+    }
+
+    createDonorWithCard(card, email, firstName, lastName) {
+        return this.apiDonor(card, email, firstName, lastName, stripe.card, 'POST');
+    }
+
+    updateDonorWithBankAcct(donorId, bankAcct, email) {
+        return this.apiDonor(bankAcct, email, null, null, stripe.bankAccount, 'PUT');
+    }
+
+    updateDonorWithCard(donorId, card, email) {
+        return this.apiDonor(card, email, null, null, stripe.card, 'PUT');
+    }
+
+    apiDonor(donorInfo, email, firstName, lastName, stripeFunc, apiMethod): Observable<string> {
+        let def = $q.defer();
+        stripeFunc.createToken(donorInfo, function (status, response) {
+            if (response.error) {
+                def.reject(_addGlobalErrorMessage(response.error, status));
+            } else {
+
+                //TODO - need this object - and add to it???
+                let donorRequest = { stripe_token_id: response.id, email_address: email, first_name: firstName, last_name: lastName };
+
+                // $http({
+                //     method: apiMethod,
+                //     url: this.transactionUrl + 'api/donor',
+                //     headers: {
+                //         Authorization: $cookies.get(cookieNames.SESSION_ID)
+                //     },
+                //     data: donorRequest
+                // }).success(function (data) {
+                //     this.donor = data;
+                //     def.resolve(data);
+                // }).error(function (response, statusCode) {
+                //     def.reject(_addGlobalErrorMessage(response.error, statusCode));
+                // });
+
+                return this.http.get(this.transactionUrl + 'api/donor')
+                    .map(this.extractDonor)
+                    .catch(this.errorDonor);
+            }
+        });
+    }
+
+    stripeErrorHandler(error) {
+        if (error && error.globalMessage) {
+            GiveTransferService.declinedPayment =
+                error.globalMessage.id === $rootScope.MESSAGES.paymentMethodDeclined.id;
+            $rootScope.$emit('notify', error.globalMessage);
+        } else {
+            $rootScope.$emit('notify', $rootScope.MESSAGES.failedResponse);
+        }
+
+        GiveTransferService.processing = false;
+    }
+
+    private extractDonorEmail(res: Response) {
+        let body: any = res;
+        return '';
+    }
+
+    private errorDonorEmail(res: Response) {
+        return [res.json()];
+    }
+
+    private extractDonor(res: Response) {
+        let body: any = res;
+        return '';
+    }
+
+    private errorDonor(res: Response) {
+        return [res.json()];
+    }
+
+}
