b08b14afb7dabba43b3a52764e63feb9e3af7adf
Samuel Mello <samueljmello@gmail.com>
2016-11-10 15:15:31 -0500

    Fixing false tests

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { StateManagerService } from '../services/state-manager.service';
