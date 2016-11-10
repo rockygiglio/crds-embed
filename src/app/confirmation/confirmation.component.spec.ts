fb5587dc79c859fea555fb5da68ed37c09ce2cf9
Samuel Mello <samueljmello@gmail.com>
2016-11-10 15:42:48 -0500

    US5490: Fixed tests

--- src/app/confirmation/confirmation.component.spec.ts
+++ src/app/confirmation/confirmation.component.spec.ts
@@ -1,14 +1,29 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { ConfirmationComponent } from './confirmation.component';
+import { HttpModule, JsonpModule  } from '@angular/http';
 import { GiftService } from '../services/gift.service';
+import { ParamValidationService } from '../services/param-validation.service';
+import { DonationFundService } from '../services/donation-fund.service';
+import { QuickDonationAmountsService } from '../services/quick-donation-amounts.service';
+import { UserSessionService } from '../services/user-session.service';
+import { PreviousGiftAmountService } from '../services/previous-gift-amount.service';
+import { ExistingPaymentInfoService } from '../services/existing-payment-info.service';
 
+class MockDonationService { }
+class MockQuickDonationAmountsService { }
+class MockUserSessionService { }
+class MockPreviousGiftAmountService { }
+class MockExistingPaymentInfoService { }
+class MockGiftService { }
+class MockRouter { public navigate() {}; }
 class MockPrototypeStore { public subscribe() {}; }
 
-describe('Component: PrototypeConfirmation', () => {
+describe('Component: Confirmation', () => {
 
   let component: any;
   let fixture: any;
@@ -17,10 +32,17 @@ describe('Component: PrototypeConfirmation', () => {
     TestBed.configureTestingModule({
       declarations: [ ConfirmationComponent ],
       imports: [
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        RouterTestingModule.withRoutes([]), HttpModule
       ],
       providers:    [
-       GiftService
+        { provide: DonationFundService, useClass: MockDonationService },
+        { provide: QuickDonationAmountsService, useClass: MockQuickDonationAmountsService },
+        { provide: UserSessionService, useClass: MockUserSessionService },
+        { provide: PreviousGiftAmountService, useClass: MockPreviousGiftAmountService },
+        { provide: ExistingPaymentInfoService, useClass: MockExistingPaymentInfoService },
+        { provide: GiftService, useClass: MockGiftService },
+        ParamValidationService
       ]
     });
     this.fixture = TestBed.createComponent(ConfirmationComponent);
