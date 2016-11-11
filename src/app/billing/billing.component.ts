eb4e4f3a19d49abf1dac01f30dc6a2d01dbd6b81
Samuel Mello <samueljmello@gmail.com>
2016-11-11 15:38:12 -0500

    whoopsie

--- src/app/billing/billing.component.ts
+++ src/app/billing/billing.component.ts
@@ -55,7 +55,7 @@ export class BillingComponent implements OnInit {
       accountName: ['', [<any>Validators.required]],
       routingNumber: ['', [<any>Validators.required, <any>Validators.minLength(9), <any>Validators.maxLength(9)]],
       accountNumber: ['', [<any>Validators.required, <any>Validators.minLength(4), <any>Validators.maxLength(30)]],
-      accountType:   ['this.gift.accountType', [<any>Validators.required]]
+      accountType:   ['', [<any>Validators.required]]
     });
 
     this.ccForm = this.fb.group({
