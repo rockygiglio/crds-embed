a2f266301c999aed9bf3d59be44e99fc0b7ea108
vchtelmakh <vchtelmakh@callibrity.com>
2016-11-17 08:36:54 -0500

    Wait for service data to be resolved until showing funds page

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -8,12 +8,18 @@ import { SummaryComponent } from './summary/summary.component';
 import { ConfirmationComponent } from './confirmation/confirmation.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
 import { FundAndFrequencyComponent } from './fund-and-frequency/fund-and-frequency.component'
+import { DonationFundService } from './services/donation-fund.service';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/payment', pathMatch: 'full' },
   { path: 'payment', component: PaymentComponent },
   { path: 'donation', component: DonationComponent },
-  { path: 'fund', component: FundAndFrequencyComponent },
+  { path: 'fund',
+    component: FundAndFrequencyComponent,
+    resolve: {
+      giveTo: DonationFundService
+    }
+  },
   { path: 'auth', component: AuthenticationComponent },
   { path: 'billing', component: BillingComponent },
   { path: 'summary', component: SummaryComponent },
