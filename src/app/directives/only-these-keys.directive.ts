6f2a6ee4642511fcdc149785c63100010fed1b2a
Samuel Mello <samueljmello@gmail.com>
2016-11-18 10:20:25 -0500

    DE2218: Add generic directive for handling custo regex

---
+++ src/app/directives/only-these-keys.directive.ts
@@ -0,0 +1,50 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[onlyTheseKeys]'
+})
+
+export class OnlyTheseKeysDirective {
+
+  public target;
+  public regex: RegExp = new RegExp('[0-9\.]');
+  public custom: string;
+
+  @Input('onlyTheseKeys') onlyTheseKeys: any;
+
+  constructor(private el: ElementRef) {
+    this.target = this.el.nativeElement;
+  }
+
+  @HostListener('keypress', ['$event']) onKeypress(e) {
+
+    let input;
+    let keypress = e.which;
+    if (!keypress && e.charCode) {
+      keypress = e.charCode;
+    }
+
+    if (e.metaKey || e.ctrlKey) {
+      return true;
+    }
+    if (keypress === 32) {
+      return false;
+    }
+    if (keypress === 0) {
+      return true;
+    }
+    if (keypress < 33) {
+      return true;
+    }
+
+    if (this.onlyTheseKeys && !this.custom) {
+      this.custom = this.onlyTheseKeys;
+      this.regex = new RegExp(this.custom);
+    }
+
+    input = String.fromCharCode(keypress);
+    return this.regex.test(input);
+
+  }
+
+}
