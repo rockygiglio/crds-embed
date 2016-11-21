4359c8cadb3ace13acde9d57b517c963805459f0
Michael Vezzani <mvezzani@callibrity.com>
2016-11-21 14:35:54 -0500

    Pass amount to stripe without a decimal.

--- src/app/models/payment-call-body.ts
+++ src/app/models/payment-call-body.ts
@@ -7,11 +7,14 @@ export class PaymentCallBody {
     invoice_id: number;
 
     constructor(amount: number, pymt_type: string, transaction_type: string, invoice_id: number) {
-        this.amount = amount;
+        this.amount = this.formatAmountForStripe(amount);
         this.pymt_type = pymt_type;
         this.transaction_type = transaction_type;
         this.invoice_id = invoice_id;
     }
 
+    formatAmountForStripe(amount: any): any {
+        return Number(amount.toString().replace(".", ""));
+    }
 }
 
