fc5dbf7ccdeb30b4b6a4e7037d2ae5e814017e9f
Michael Vezzani <mvezzani@callibrity.com>
2016-11-23 17:02:20 -0500

    Fix issues with highlighted text in cc field in firefox.

--- src/app/directives/simple-credit-card-format.directive.ts
+++ src/app/directives/simple-credit-card-format.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
+import { KeypressValidation } from '../shared/keypress-validation';
 
 @Directive({
   selector: '[simpleCcNumber]'
@@ -13,11 +14,40 @@ export class SimpleCreditCardFormatDirective {
     this.target = this.el.nativeElement;
   }
 
-  @HostListener('keypress', ['$event']) onKeyup(e) {
-    this.position = this.target.selectionStart;
+  @HostListener('paste', ['$event']) onPaste(e) {
+    if (KeypressValidation.isNonValidationKeypress(e)) {
+      return true;
+    }
+    if ( this.isValidInput(e.clipboardData.getData('Text')) ) {
+      this.inputData(e);
+    } else {
+      return false;
+    }
+  }
 
-    this.target.value = this.target.value.replace(/[^0-9]/g, '');
-    this.target.selectionEnd = this.position;
+  @HostListener('keypress', ['$event']) onKeypress(e) {
+    if (KeypressValidation.isNonValidationKeypress(e)) {
+      return true;
+    }
+    if ( this.isValidInput(String.fromCharCode(e.which)) ) {
+      this.inputData(e);
+    } else {
+      e.preventDefault();
+      return false;
+    }
+  }
+
+  private isValidInput(input: string): boolean {
+    let val = KeypressValidation.replaceFullWidthChars(this.target.value);
+
+    val = val.substring(0, this.target.selectionStart) + input + val.substring(this.target.selectionEnd);
+    let regex: RegExp = new RegExp('[0-9]');
+    return regex.test(val);
+  }
+
+  private inputData(target): void {
+    this.position = target.selectionStart;
+    target.value = target.value.replace(/[^0-9]/g, '');
+    target.selectionEnd = this.position
   }
 }
-
