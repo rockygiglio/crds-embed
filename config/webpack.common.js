677166a73765fed28343fa2dd76936402d117f24
Richard Barker <rbarker@Administrators-MacBook-Pro-2.local>
2016-12-06 13:18:29 -0500

    WIP try to replace strings in webpack

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -4,12 +4,14 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var helpers = require('./helpers');
 var Dotenv = require('dotenv-webpack');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var StringReplacePlugin = require("string-replace-webpack-plugin");
 
 module.exports = {
   entry: {
     'polyfills': ['./src/polyfills.ts'],
     'vendor': ['./src/vendor.ts'],
-    'app': ['./src/main.ts']
+    'app': ['./src/main.ts'],
+    'apache_site': ['./apache_site.conf']
   },
 
   resolve: {
@@ -26,13 +28,14 @@ module.exports = {
         test: /\.html$/,
         loader: 'html'
       },
-      { 
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-        loader: "url?limit=10000&mimetype=application/font-woff" 
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
       },
-      { 
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-        loader: "file" },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file"
+      },
       {
         test: /\.(png|jpe?g|gif|svg|ico)$/,
         loader: 'file?name=assets/[name].[hash].[ext]'
@@ -51,15 +54,29 @@ module.exports = {
         test: /\.scss$/,
         exclude: /node_modules/,
         loaders: ['raw-loader', 'sass-loader']
+      },
+      {
+        test: /apache_site\.conf$/,
+        loader: StringReplacePlugin.replace({
+          replacements: [
+            {
+              pattern: /\${(.*)}/g,
+              replacement: function (match, p1, offset, string) {
+                console.log(eval('process.env.' + p1).toString());
+                return eval('process.env.' + p1).toString();
+              }
+            }
+          ]
+        })
       }
     ]
   },
 
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
-      name: ['app', 'vendor', 'polyfills']
+      name: ['app', 'vendor', 'polyfills', 'apache_site']
     }),
-    
+
     new Dotenv({
       systemvars: true
     }),
@@ -72,10 +89,12 @@ module.exports = {
       from: 'src/assets',
       to: 'assets',
     }], { ignore: ['*.scss', 'mock-data/*'] }),
-    
+
     new CopyWebpackPlugin([{
       from: './apache_site.conf',
       to: 'apache_site.conf',
-    }])
+    }]),
+
+    new StringReplacePlugin()
   ]
 };
