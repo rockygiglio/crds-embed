68d3271aba7b9b4cd76fb53c3c11df6f4094fe0e
Michael Vezzani <mvezzani@callibrity.com>
2016-12-08 16:01:11 -0500

    WIP: Refactor unit tests for billing.component

--- src/app/billing/billing.component.spec.ts
+++ src/app/billing/billing.component.spec.ts
@@ -1,22 +1,23 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BillingComponent } from './billing.component';
-import { GiftService } from '../services/gift.service';
-import { RouterTestingModule } from '@angular/router/testing';
-import { HttpModule, JsonpModule } from '@angular/http';
-import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
-import { ExistingPaymentInfoService } from '../services/existing-payment-info.service';
-import { AlertModule, CollapseModule, TabsModule, ButtonsModule } from 'ng2-bootstrap/ng2-bootstrap';
 import { ActivatedRoute } from '@angular/router';
-import { HttpClientService } from '../services/http-client.service';
+import { AlertModule, ButtonsModule, CollapseModule, TabsModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { BillingComponent } from './billing.component';
 import { CookieService } from 'angular2-cookie/services/cookies.service';
-import { ParamValidationService } from '../services/param-validation.service';
+import { CreditCardValidator } from '../validators/credit-card.validator';
 import { DonationFundService } from '../services/donation-fund.service';
-import { QuickDonationAmountsService } from '../services/quick-donation-amounts.service';
-import { PreviousGiftAmountService } from '../services/previous-gift-amount.service';
-import { StateManagerService } from '../services/state-manager.service';
+import { ExistingPaymentInfoService } from '../services/existing-payment-info.service';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { GiftService } from '../services/gift.service';
+import { HttpClientService } from '../services/http-client.service';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { ParamValidationService } from '../services/param-validation.service';
 import { PaymentService } from '../services/payment.service';
+import { PreviousGiftAmountService } from '../services/previous-gift-amount.service';
+import { QuickDonationAmountsService } from '../services/quick-donation-amounts.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { StateManagerService } from '../services/state-manager.service';
 import { StripeService } from '../services/stripe.service';
 
 class MockDonationFundService { }
@@ -27,6 +28,28 @@ class MockActivatedRoute {
   public snapshot = {
     queryParams: []
   };
+}
+
+export class TestHelper {
+  static setAchForm(achForm: any, name: string, routing: number, account: number) {
+    achForm.setValue({accountName: name});
+    achForm.controls['accountName'].markAsTouched();
+    achForm.setValue({accountNumber: account});
+    achForm.controls['accountNumber'].markAsTouched();
+    achForm.setValue({routingNumber: routing});
+    achForm.controls['routingNumber'].markAsTouched();
+  }
+
+  static setCcForm(ccForm: any, ccNumber: number, exp: string, cvv: number, zip: number) {
+    ccForm.setValue({ccNumber: ccNumber});
+    ccForm.controls['ccNumber'].markAsTouched();
+    ccForm.setValue({expDate: exp});
+    ccForm.controls['expDate'].markAsTouched();
+    ccForm.setValue({cvv: cvv});
+    ccForm.controls['cvv'].markAsTouched();
+    ccForm.setValue({zipCode: zip});
+    ccForm.controls['zipCode'].markAsTouched();
+  }
 }
 
 describe('Component: Billing', () => {
@@ -65,38 +88,47 @@ describe('Component: Billing', () => {
     this.component = this.fixture.componentInstance;
   }));
 
-
   it('should create an instance', () => {
     expect(this.component).toBeTruthy();
   });
 
-  it('should validate required ACH parameters', () => {
-    this.component.achForm = {
-      valid: false,
-      controls: {
-        accountName: { valid: false, errors: { required: true } },
-        accountNumber: { valid: false, errors: { minLength: 8, requiredLength: 9 } },
-        routingNumber: { valid: true, errors: null }
-      }
-    };
-    expect(this.component.achForm.controls['accountName'].valid).toBe(false);
-    expect(this.component.achForm.controls['accountNumber'].valid).toBe(false);
-    expect(this.component.achForm.controls['routingNumber'].valid).toBe(true);
+  describe('ACH Form', () => {
+    it('should be valid with required parameters provided', () => {
+      TestHelper.setAchForm(this.component.achForm, 'Bob Dillinger', 110000000, 123123456789);
+      this.component.achNext();
+      expect(this.component.achForm.valid).toBe(true);
+    });
+
+    it('should be invalid with required parameters not provided', () => {
+      TestHelper.setAchForm(this.component.achForm, '', null, null);
+      this.component.achNext();
+      expect(this.component.achForm.valid).toBe(false);
+    });
+
+    it('should be invalid with required parameters partially provided', () => {
+      TestHelper.setAchForm(this.component.achForm, 'Bob Dillinger', 1100, 12345); 
+      this.component.achNext();
+      expect(this.component.achForm.valid).toBe(false);
+    });
   });
 
-  it('should validate required CC parameters', () => {
-    this.component.ccForm = {
-      valid: false,
-      controls: {
-        ccNumber: { valid: false, errors: { required: true } },
-        expDate: { valid: false, errors: { minLength: 8, requiredLength: 9 } },
-        cvv: { valid: true, errors: null },
-        zipCode: { valid: true, errors: null }
-      }
-    };
-    expect(this.component.ccForm.controls['ccNumber'].valid).toBe(false);
-    expect(this.component.ccForm.controls['expDate'].valid).toBe(false);
-    expect(this.component.ccForm.controls['cvv'].valid).toBe(true);
-    expect(this.component.ccForm.controls['zipCode'].valid).toBe(true);
+  describe('CC Form', () => {
+    fit('should be valid with required parameters provided', () => {
+      TestHelper.setCcForm(this.component.ccForm, 4242424242424242, '09/31', 345, 34567);
+      console.log("HELLO?>>>>>>????????????????????????");
+      expect(this.component.ccForm.valid).toBe(true);
+    });
+
+    it('should be invalid with required parameters not provided', () => {
+      TestHelper.setCcForm(this.component.ccForm, null, null, null, null);
+      this.component.ccNext();
+      expect(this.component.ccForm.valid).toBe(false);
+    });
+
+    it('should be invalid with required parameters partially provided', () => {
+      TestHelper.setCcForm(this.component.ccForm, 424242, '02/', 34, 234);
+      this.component.ccNext();
+      expect(this.component.ccForm.valid).toBe(false);
+    });
   });
 });
