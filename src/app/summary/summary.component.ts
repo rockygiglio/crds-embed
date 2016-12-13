e6985532bc8a550f7ec7ccce3b9aa3387d16da39
Samuel Mello <samueljmello@gmail.com>
2016-12-12 22:00:52 -0500

    US5978: Fix styles

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -14,7 +14,7 @@ export function _window(): Window {
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
-  styleUrls: ['./summary.component.css']
+  styleUrls: ['./summary.component.scss']
 })
 export class SummaryComponent implements OnInit {
 
