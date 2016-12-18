b7c77d6d9c07a16740d6d3afa8e524f660ca5dc5
Phil Lachmann <phil.lachmann@ingagepartners.com>
2016-12-18 11:36:23 -0500

    add unit tests for content service

---
+++ src/app/services/content.service.spec.ts
@@ -0,0 +1,62 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async, inject } from '@angular/core/testing';
+import { SessionService } from './session.service';
+import { MockBackend } from '@angular/http/testing';
+import { BaseRequestOptions, Http, HttpModule, Response, ResponseOptions, RequestOptions, Headers } from '@angular/http';
+import { CookieService } from 'angular2-cookie/core';
+import { ContentService } from './content.service';
+import { ContentBlock } from '../models/content-block';
+
+describe('Service: Content', () => {
+
+  const mockContent1 = {
+    'id': 1,
+    'title': 'sampleContentBlockTitle1',
+    'content': 'blah blah blah',
+    'type': 'error',
+    'category': 'mycategory',
+    'className': 'myclassname'
+  };
+
+  const mockContent2 = {
+    'id': 1,
+    'title': 'sampleContentBlockTitle2',
+    'content': 'more blah blah blah',
+    'type': 'error',
+    'category': 'mycategory',
+    'className': 'myclassname'
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        ContentService,
+        MockBackend,
+        BaseRequestOptions,
+        CookieService,
+        {
+          provide: Http,
+          useFactory: (backend, options) => new Http(backend, options),
+          deps: [MockBackend, BaseRequestOptions]
+        }]
+    });
+  });
+
+  fit('should create an instance', inject([ContentService], (service: ContentService) => {
+    expect(service).toBeTruthy();
+  }));
+
+  fit('should get content', inject([ContentService], (service: ContentService) => {
+    let title = 'sampleContentBlockTitle1';
+    service.contentBlocks = new Array<ContentBlock>();
+    service.contentBlocks.push(mockContent1);
+    service.contentBlocks.push(mockContent2);
+
+    let content = service.getContent(title);
+
+    expect(content).toBeDefined;
+    expect(content).toBe('blah blah blah');
+  }));
+
+});
