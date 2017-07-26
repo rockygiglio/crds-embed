5e9e8cdd7e133feb239fd93edee5ec0ff569e098
dshannon@callibrity.com <dshannon@callibrity.com>
2017-07-26 12:48:51 -0400

    fixed unit test, fixed example call

--- src/app/services/analytics.service.ts
+++ src/app/services/analytics.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Angulartics2Segment } from 'angulartics2';
+import { Angulartics2 } from 'angulartics2';
 
 @Injectable()
 export class AnalyticsService {
 
- private analytics: Angulartics2Segment;
-  constructor(private angulartics2Segment: Angulartics2Segment ) {
-    this.analytics = angulartics2Segment;
+ private analytics: Angulartics2;
+  constructor(private angulartics2: Angulartics2 ) {
+    this.analytics = angulartics2;
   }
   // EXAMPLE OF ANGULARTICS EVENT CALL
   // testCall(){
-  //   this.analytics.eventTrack("THIS IS AN EMBED TEST", {});
+  //   this.analytics.eventTrack.next({action: "THIS IS AN EMBED TEST", properties: {}});
   // }
 
 }
